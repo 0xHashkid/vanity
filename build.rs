ba92de98819184654bbe2af064360327105865a0
Hashkid <0xHashkid@proton.me>
2025-06-06 13:47:38 +0300

    Added support for more GPUs

--- build.rs
+++ build.rs
@@ -14,8 +14,18 @@ fn build_cuda_libs() {
         .file("kernels/base58.cu")
         .file("kernels/sha256.cu")
         .flag("-cudart=static")
-        .flag("-gencode=arch=compute_89,code=sm_89")
         .flag("-gencode=arch=compute_89,code=compute_89")
+        .flag("-gencode=arch=compute_75,code=sm_75")  // Turing RTX
+        .flag("-gencode=arch=compute_80,code=sm_80")  // Ampere (A100, RTX 30××)
+        .flag("-gencode=arch=compute_86,code=sm_86")  // Ampere (RTX 30×× Ti)
+        .flag("-gencode=arch=compute_89,code=sm_89")  // Ada Lovelace (RTX 40××)
+        .flag("-gencode=arch=compute_90,code=sm_90")  // Hopper (H100)
+        .flag("-gencode=arch=compute_75,code=compute_75")
+        .flag("-gencode=arch=compute_80,code=compute_80")
+        .flag("-gencode=arch=compute_86,code=compute_86")
+        .flag("-gencode=arch=compute_89,code=compute_89")
+        .flag("-gencode=arch=compute_90,code=compute_90")
+    
         .compile("libvanity.a");
 
     // Add link directory
